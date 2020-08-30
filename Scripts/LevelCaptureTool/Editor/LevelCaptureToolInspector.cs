9207c06a3daa2fba5b938ad1743a46d51884e0e2
Lech Różański <l.rozanski88@gmail.com>
2020-08-30 16:02:47 +0200

    Initial commit

---
+++ Scripts/LevelCaptureTool/Editor/LevelCaptureToolInspector.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+// ReSharper disable once CheckNamespace
+namespace LevelCaptureTool.Editor {
+
+    [CustomEditor(typeof(LevelCaptureTool))]
+    public class LevelCaptureToolInspector : UnityEditor.Editor {
+
+        private const int ActionBoxPadding = 5;
+
+        private static readonly HashSet<EventType> AcceptedEventTypes = new HashSet<EventType> {
+            EventType.MouseDown,
+            EventType.MouseDrag,
+            EventType.MouseUp,
+            EventType.KeyDown
+        };
+
+        private static readonly Color ToggleBgColor = new Color(1f, 0.85f, 0.4f);
+        private static readonly Color ToggleTextColor = new Color(1f, 0.96f, 0.56f);
+        private static readonly Color SelectionBgColor = new Color(0.07f, 0f, 0f, 0.4f);
+
+        private GUIStyle _actionBoxStyle;
+        private GUIStyle _toggleStyle;
+
+        private bool _selectionMode;
+        private bool _drawGizmos;
+        private Vector2 _start;
+        private Vector2 _end;
+
+        private void OnSceneGUI() {
+            if (!_selectionMode) {
+                return;
+            }
+            serializedObject.Update();
+
+            var levelCaptureTool = Selection.activeGameObject.GetComponent<LevelCaptureTool>();
+            var bounds = FindBounds();
+            DrawHandles(levelCaptureTool, bounds);
+            HandleUtility.AddDefaultControl(GUIUtility.GetControlID(GetHashCode(), FocusType.Passive));
+
+            var currentEvent = Event.current;
+            var use = AcceptedEventTypes.Contains(currentEvent.type);
+            var drawGizmosProperty = serializedObject.FindProperty("drawGizmos");
+            var worldPos = HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition).origin;
+
+            switch (currentEvent.type) {
+                case EventType.MouseDown:
+                    _start = worldPos;
+                    _end = worldPos;
+                    _drawGizmos = drawGizmosProperty.boolValue;
+
+                    ToggleGizmos(drawGizmosProperty, false);
+                    break;
+                case EventType.MouseDrag:
+                    _end = worldPos;
+                    break;
+                case EventType.MouseUp:
+                    UpdateCaptureRect(levelCaptureTool, bounds);
+                    FinishSelection(drawGizmosProperty);
+                    ToggleGizmos(drawGizmosProperty, _drawGizmos);
+                    break;
+                case EventType.KeyDown when currentEvent.keyCode == KeyCode.Escape:
+                    FinishSelection(drawGizmosProperty);
+                    ToggleGizmos(drawGizmosProperty, _drawGizmos);
+                    break;
+            }
+            if (use) {
+                serializedObject.ApplyModifiedProperties();
+                currentEvent.Use();
+            }
+        }
+
+        private static void ToggleGizmos(SerializedProperty drawGizmosProperty, bool enabled) {
+            drawGizmosProperty.boolValue = enabled;
+        }
+
+        private void FinishSelection(SerializedProperty drawGizmosProperty) {
+            drawGizmosProperty.boolValue = _drawGizmos;
+            _selectionMode = false;
+            _start = _end;
+
+            GUIUtility.hotControl = 0;
+        }
+
+        private Rect FindBounds() {
+            var minX = Mathf.Min(_start.x, _end.x);
+            var minY = Mathf.Min(_start.y, _end.y);
+            var maxX = Mathf.Max(_start.x, _end.x);
+            var maxY = Mathf.Max(_start.y, _end.y);
+
+            return new Rect(minX, minY, maxX - minX, maxY - minY);
+        }
+
+        private void DrawHandles(LevelCaptureTool levelCaptureTool, Rect bounds) {
+            if (_start == _end) {
+                return;
+            }
+            Handles.color = Color.green;
+            Handles.DrawWireCube(bounds.center, new Vector3(bounds.size.x + levelCaptureTool.Margin, bounds.size.y + levelCaptureTool.Margin));
+            Handles.color = Color.white;
+            Handles.DrawSolidRectangleWithOutline(bounds, SelectionBgColor, Color.red);
+
+            HandleUtility.Repaint();
+        }
+
+        private void UpdateCaptureRect(LevelCaptureTool levelCaptureTool, Rect bounds) {
+            if (_start == _end) {
+                return;
+            }
+            levelCaptureTool.transform.position = new Vector3(
+                bounds.center.x,
+                bounds.center.y,
+                levelCaptureTool.transform.position.z
+            );
+            serializedObject.FindProperty("size").vector2Value = new Vector2(
+                Mathf.Abs(bounds.size.x),
+                Mathf.Abs(bounds.size.y)
+            );
+            levelCaptureTool.MarkDirty();
+        }
+
+        public override void OnInspectorGUI() {
+            if (_actionBoxStyle == null) {
+                _actionBoxStyle = new GUIStyle(GUI.skin.box) {
+                    padding = new RectOffset(ActionBoxPadding, ActionBoxPadding, ActionBoxPadding, ActionBoxPadding)
+                };
+            }
+            if (_toggleStyle == null) {
+                _toggleStyle = new GUIStyle(GUI.skin.button) {
+                    active = {textColor = ToggleTextColor},
+                    hover = {textColor = ToggleTextColor},
+                    normal = {textColor = ToggleTextColor},
+                    focused = {textColor = ToggleTextColor},
+                    onActive = {textColor = ToggleTextColor},
+                    onHover = {textColor = ToggleTextColor},
+                    onNormal = {textColor = ToggleTextColor},
+                    onFocused = {textColor = ToggleTextColor}
+                };
+            }
+            EditorGUI.BeginChangeCheck();
+            serializedObject.UpdateIfRequiredOrScript();
+            var iterator = serializedObject.GetIterator();
+            for (var enterChildren = true; iterator.NextVisible(enterChildren); enterChildren = false) {
+                if ("drawGizmos" == iterator.propertyPath || "saveLayersSeparately" == iterator.propertyPath) {
+                    continue;
+                }
+                using (new EditorGUI.DisabledScope("m_Script" == iterator.propertyPath)) {
+                    EditorGUILayout.PropertyField(iterator, true);
+                }
+            }
+            if (EditorGUI.EndChangeCheck()) {
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            CreateOptionsBox();
+            CreateActionsBox();
+        }
+
+        private void CreateOptionsBox() {
+            GUILayout.Space(8f);
+            GUILayout.BeginVertical(_actionBoxStyle);
+            GUILayout.Label("Options", EditorStyles.boldLabel);
+
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("drawGizmos"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("saveLayersSeparately"));
+            if (EditorGUI.EndChangeCheck()) {
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            GUILayout.EndVertical();
+        }
+
+        private void CreateActionsBox() {
+            GUILayout.Space(4f);
+            GUILayout.BeginVertical(_actionBoxStyle);
+            GUILayout.Label("Actions", EditorStyles.boldLabel);
+
+            GUILayout.BeginHorizontal();
+            var freeformButtonContent = new GUIContent("Select", EditorGUIUtility.ObjectContent(null, typeof(BoxCollider2D)).image);
+
+            GUI.backgroundColor = ToggleBgColor;
+            _selectionMode = GUILayout.Toggle(_selectionMode, freeformButtonContent, _toggleStyle, GUILayout.Width(90f), GUILayout.Height(24f));
+            GUI.backgroundColor = Color.white;
+
+            var instance = ((LevelCaptureTool) serializedObject.targetObject);
+            EditorGUI.BeginChangeCheck();
+            if (GUILayout.Button("Trim Bounds", GUILayout.Height(24f))) {
+                instance.TrimBounds();
+                GUIUtility.ExitGUI();
+            }
+            if (GUILayout.Button("Update Camera", GUILayout.Height(24f))) {
+                instance.UpdateCamera();
+                GUIUtility.ExitGUI();
+            }
+            if (GUILayout.Button("Capture", GUILayout.Height(24f))) {
+                instance.Capture();
+                GUIUtility.ExitGUI();
+            }
+            if (EditorGUI.EndChangeCheck()) {
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            GUILayout.EndHorizontal();
+            GUILayout.EndVertical();
+        }
+    }
+}
---
+++ Scripts/LevelCaptureTool/LevelCaptureTool.cs
@@ -0,0 +1,280 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace LevelCaptureTool {
+
+    [ExecuteAlways]
+    [RequireComponent(typeof(Camera))]
+    [SuppressMessage("ReSharper", "RedundantDefaultMemberInitializer")]
+    public class LevelCaptureTool : MonoBehaviour {
+
+        private readonly Color _selectionBgColor = new Color(0.25f, 0f, 0f, 0.25f);
+
+        public Vector2 Size {
+            get => size;
+            set {
+                size = value;
+                _modified = true;
+            }
+        }
+
+        public float Margin {
+            get => margin;
+            set {
+                margin = value;
+                _modified = true;
+            }
+        }
+
+        [SerializeField]
+        private RenderTexture renderTexture = default;
+
+        [SerializeField]
+        private int pixelsPerUnit;
+
+        [SerializeField]
+        private LayerMask layerMask = default;
+
+        [SerializeField, UsePropertyInInspector(nameof(Size))]
+        private Vector2 size;
+
+        [SerializeField, UsePropertyInInspector(nameof(Margin))]
+        private float margin;
+
+        [SerializeField]
+        private float zOffset = -10f;
+
+        public bool drawGizmos = true;
+
+        [SerializeField]
+        private bool saveLayersSeparately = default;
+
+        private Camera _camera;
+        private Bounds _bounds;
+        private bool _modified;
+
+        public void MarkDirty() => _modified = true;
+
+        private void OnDisable() => ClearTexture();
+        private void Awake() => _camera = GetComponent<Camera>();
+        private void Start() => _bounds = new Bounds(transform.position, size);
+
+        private void ClearTexture() {
+            if (renderTexture != null) {
+                renderTexture.Release();
+            }
+        }
+
+        private void Update() {
+            _bounds.center = transform.position;
+            if (!_modified) {
+                return;
+            }
+            _bounds.size = size;
+            UpdateCamera();
+            EditorUtility.SetDirty(gameObject);
+        }
+
+        public void TrimBounds() {
+            var colliders = Physics2D.OverlapAreaAll(_bounds.min, _bounds.max, layerMask);
+            if (colliders == null || colliders.Length == 0) {
+                Debug.Log("No matching colliders found in the selected area");
+                return;
+            }
+
+            var rect = FindColliderRect(colliders);
+            if (!rect.HasValue) {
+                return;
+            }
+            var rectCenter = rect.Value.center;
+
+            var rectSize = rect.Value.size;
+            if (_bounds.size.x < rectSize.x || _bounds.size.y < rectSize.y) {
+                return;
+            }
+            transform.position = new Vector3(rectCenter.x, rectCenter.y, zOffset);
+            _bounds.center = rectCenter;
+            _bounds.size = rectSize;
+            size = _bounds.size;
+
+            _modified = false;
+            EditorUtility.SetDirty(gameObject);
+            UpdateCamera();
+        }
+
+        private static Rect? FindColliderRect(Collider2D[] colliders) {
+            var minX = float.MaxValue;
+            var maxX = float.MinValue;
+            var minY = float.MaxValue;
+
+            var maxY = float.MinValue;
+
+            // ReSharper disable once LocalVariableHidesMember
+            foreach (var collider in colliders) {
+                minX = Mathf.Min(minX, collider.bounds.min.x);
+                maxX = Mathf.Max(maxX, collider.bounds.max.x);
+                minY = Mathf.Min(minY, collider.bounds.min.y);
+                maxY = Mathf.Max(maxY, collider.bounds.max.y);
+            }
+            if (minX >= float.MaxValue || maxX <= float.MinValue || minY >= float.MaxValue || maxY <= float.MinValue) {
+                return null;
+            }
+            return new Rect(minX, minY, maxX - minX, maxY - minY);
+        }
+
+        public void UpdateCamera() {
+            ClearTexture();
+            UpdateZPosition();
+
+            var maxSize = Mathf.Max(_bounds.size.x + margin, _bounds.size.y + margin) * pixelsPerUnit;
+            var textureSize = CeilPower2((int) maxSize);
+            renderTexture = new RenderTexture(textureSize, textureSize, 0, RenderTextureFormat.Default, 0);
+
+            _camera = GetComponent<Camera>();
+            _camera.aspect = 1f;
+            _camera.orthographicSize = textureSize / (float) pixelsPerUnit / 2f;
+            _camera.targetTexture = renderTexture;
+        }
+
+        public void Capture() {
+            var path = EditorUtility.SaveFilePanelInProject("Save captured level part", "", "png", "");
+            if (string.IsNullOrEmpty(path)) {
+                return;
+            }
+
+            var layerNames = new List<string>();
+            for (var i = 0; i < 32; i++) {
+                if (layerMask != (layerMask | (1 << i))) {
+                    continue;
+                }
+                var layerName = LayerMask.LayerToName(i);
+                if (string.IsNullOrEmpty(layerName)) {
+                    continue;
+                }
+                layerNames.Add(LayerMask.LayerToName(i));
+            }
+            if (saveLayersSeparately) {
+                layerNames.ForEach(layerName => SaveLayerMask(path, true, layerName));
+            } else {
+                SaveLayerMask(path, false, layerNames.ToArray());
+            }
+        }
+
+        private void SaveLayerMask(string path, bool appendLayerNames, params string[] layerNames) {
+            Debug.Log($"Capturing layers: {string.Join(", ", layerNames)}");
+            UpdateCamera();
+
+            var cameraMask = _camera.cullingMask;
+            _camera.cullingMask = LayerMask.GetMask(layerNames);
+            _camera.Render();
+            _camera.cullingMask = cameraMask;
+
+            var active = RenderTexture.active;
+            RenderTexture.active = renderTexture;
+
+            var texture = new Texture2D(renderTexture.width, renderTexture.height, renderTexture.graphicsFormat, 0, TextureCreationFlags.None);
+            texture.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0, false);
+
+            IncludeOnlyBounds(texture, _bounds);
+
+            texture.Apply();
+            RenderTexture.active = active;
+
+            if (appendLayerNames) {
+                var extensionIndex = path.LastIndexOf(".", StringComparison.Ordinal);
+                var extension = path.Substring(extensionIndex + 1);
+                var pathWithoutExtension = path.Substring(0, extensionIndex);
+                var newPath = $"{pathWithoutExtension}_{string.Join("_", layerNames)}.{extension}";
+
+                File.WriteAllBytes(newPath, texture.EncodeToPNG());
+            } else {
+                File.WriteAllBytes(path, texture.EncodeToPNG());
+            }
+            ClearTexture();
+        }
+
+        private void IncludeOnlyBounds(Texture2D texture, Bounds bounds) {
+            var texturePixelRect = new RectInt(
+                0,
+                0,
+                renderTexture.width,
+                renderTexture.height
+            );
+            var pixelBounds = new RectInt(
+                (int) (texturePixelRect.width / 2f - Mathf.Abs(bounds.size.x * pixelsPerUnit / 2f)),
+                (int) (texturePixelRect.height / 2f - Mathf.Abs(bounds.size.y * pixelsPerUnit / 2f)),
+                (int) (bounds.size.x * pixelsPerUnit),
+                (int) (bounds.size.y * pixelsPerUnit)
+            );
+            var borderRects = new[] {
+                new RectInt(0, 0, pixelBounds.xMin, texturePixelRect.height),
+                new RectInt(pixelBounds.xMin, pixelBounds.yMax, pixelBounds.width, texturePixelRect.height - pixelBounds.yMax),
+                new RectInt(pixelBounds.xMax, 0, texture.width - pixelBounds.xMax, texturePixelRect.height),
+                new RectInt(pixelBounds.xMin, 0, pixelBounds.width, pixelBounds.yMin)
+            };
+            foreach (var borderRect in borderRects) {
+                FillRect(texture, borderRect, Color.clear);
+            }
+        }
+
+        private void FillRect(Texture2D texture, RectInt texturePixelRect, Color color) {
+            var colors = new Color[texturePixelRect.width * texturePixelRect.height];
+            for (var i = 0; i < colors.Length; i++) {
+                colors[i] = color;
+            }
+            texture.SetPixels(texturePixelRect.xMin, texturePixelRect.yMin, texturePixelRect.width, texturePixelRect.height, colors);
+        }
+
+        /// <remarks>Taken from https://stackoverflow.com/a/54065600</remarks>
+        private static int CeilPower2(int x) {
+            if (x < 2) {
+                return 1;
+            }
+
+            return (int) Math.Pow(2, (int) Math.Log(x - 1, 2) + 1);
+        }
+
+        private void OnDrawGizmosSelected() {
+            if (!drawGizmos || _bounds.size == Vector3.zero) {
+                return;
+            }
+            Gizmos.color = _selectionBgColor;
+            Gizmos.DrawCube(_bounds.center, _bounds.size);
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(_bounds.center, _bounds.size);
+
+            if (margin > 0f) {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireCube(_bounds.center, new Vector3(_bounds.size.x + margin, _bounds.size.y + margin));
+            }
+        }
+
+        private void OnValidate() {
+            if (_camera == null) {
+                _camera = GetComponent<Camera>();
+            }
+            _camera.orthographic = true;
+            _camera.aspect = 1f;
+            size.x = Mathf.Max(size.x, 0f);
+            size.y = Mathf.Max(size.y, 0f);
+            pixelsPerUnit = Mathf.Max(pixelsPerUnit, 1);
+            margin = Mathf.Max(margin, 0f);
+            UpdateZPosition();
+        }
+
+        private void UpdateZPosition() {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                zOffset
+            );
+        }
+
+    }
+}
